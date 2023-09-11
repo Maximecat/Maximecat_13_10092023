a8a8375cbeb90ff6f50ad0b97ca4d8891a6930f6
Maxime CAT <maximecat@outlook.fr>
2023-09-11 14:14:29 +0200

    Feat (Profil page) : inplement Card & HeaderUser

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,9 +1,16 @@
+import BankAccountCard from '../../components/BankAccountCard/BankAccountCard';
+import HeaderUser from '../../components/HeaderUser/HeaderUser'
 import './Profil.css';
 
 function Profil() {
     return (
-        <div className="profil-main">
-        </div>
+        <main className="main bg-dark">
+            <HeaderUser firstname="Maxime" lastName="Cat" />
+            <h2 class="sr-only">Accounts</h2>
+            <BankAccountCard accountTitle="Argent Bank Checking (x8349)" accountAmount="$2,082.79" accountAmountDescription="Available Balance" />
+            <BankAccountCard accountTitle="Argent Bank Savings (x6712)" accountAmount="$10,928.42" accountAmountDescription="Available Balance" />
+            <BankAccountCard accountTitle="Argent Bank Credit Card (x8349)" accountAmount="$184.30" accountAmountDescription="Current Balance" />
+        </main>
     );
 }
 
