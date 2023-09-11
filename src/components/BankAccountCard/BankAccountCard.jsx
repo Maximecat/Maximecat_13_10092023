06a46fb62ecff24e80ebf462a90db9d56b3e48c0
Maxime CAT <maximecat@outlook.fr>
2023-09-11 14:13:42 +0200

    Feat (BankAccountCard) : create card profil page

--- src/components/BankAccountCard/BankAccountCard.jsx
+++ src/components/BankAccountCard/BankAccountCard.jsx
@@ -1,9 +1,17 @@
 import './BankAccountCard.css'
 
-function BankAccountCard() {
+function BankAccountCard({ accountTitle, accountAmount, accountAmountDescription }) {
     return (
-        <article className='bank-account-card'>
-        </article>
+        <section class="account">
+            <div class="account-content-wrapper">
+                <h3 class="account-title">{accountTitle}</h3>
+                <p class="account-amount">{accountAmount}</p>
+                <p class="account-amount-description">{accountAmountDescription}</p>
+            </div>
+            <div class="account-content-wrapper cta">
+                <button class="transaction-button">View transactions</button>
+            </div>
+        </section>
     )
 }
 
