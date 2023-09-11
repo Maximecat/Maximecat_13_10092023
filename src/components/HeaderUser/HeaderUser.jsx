1ca863c265da3fc730480a13b579e8978df2752d
Maxime CAT <maximecat@outlook.fr>
2023-09-11 14:12:09 +0200

    Feat (HeaderUser) : inplement in profil page

---
+++ src/components/HeaderUser/HeaderUser.jsx
@@ -0,0 +1,13 @@
+
+import './HeaderUser.css';
+
+function HeaderUser(firstname, lastname) {
+    return (
+        <div class="header">
+            <h1>Welcome back<br />{firstname + ' ' + lastname}</h1>
+            <button class="edit-button">Edit Name</button>
+        </div>
+    );
+}
+
+export default HeaderUser;
