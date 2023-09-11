d869ed1ec71d5c437a8d8f9f61058ade2a7f7377
Maxime CAT <maximecat@outlook.fr>
2023-09-11 10:48:33 +0200

    Feat (SignInForm) : init component

---
+++ src/components/SignInForm/SignInForm.jsx
@@ -0,0 +1,11 @@
+import './SignInForm.css'
+
+function SignInForm() {
+    return (
+        <div className='sign-in-form-container'>
+            <form></form>
+        </div>
+    )
+}
+
+export default SignInForm;
