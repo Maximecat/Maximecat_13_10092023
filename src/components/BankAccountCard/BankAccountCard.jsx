017b0fddedd00d95d52322a7a856353dfe45ca44
Maxime CAT <maximecat@outlook.fr>
2023-09-11 10:47:24 +0200

    Feat (BankAccountCard) : init component

---
+++ src/components/BankAccountCard/BankAccountCard.jsx
@@ -0,0 +1,10 @@
+import './BankAccountCard.css'
+
+function BankAccountCard() {
+    return (
+        <article className='bank-account-card'>
+        </article>
+    )
+}
+
+export default BankAccountCard;
