58b28441b83e8c4681d2eef5c3512144090bce75
Maxime CAT <maximecat@outlook.fr>
2023-11-29 12:37:33 +0100

    SignIn page, add Header/Footer

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,17 @@
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 import SignInForm from '../../components/SignInForm/SignInForm';
 import './SignIn.css';
 
 function SignIn() {
     return (
-        <main className="main bg-dark">
-            <SignInForm />
-        </main>
+        <>
+            <Header />
+            <main className="main bg-dark">
+                <SignInForm />
+            </main>
+            <Footer />
+        </>
     );
 }
 
