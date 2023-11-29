536e6d7173737a6b5b55324acb79ecb0f91039f2
Maxime CAT <maximecat@outlook.fr>
2023-11-29 12:19:26 +0100

    component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import './Header.css'
+import argentBankLogo from '../../pages/Home/argentBankLogo.png';
 
 function Header() {
     return (
-        <header className='header'>
-        </header>
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="/">
+                <img
+                    className="main-nav-logo-image"
+                    src={argentBankLogo}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item" href="/login">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+            </div>
+        </nav>
     )
 }
 
