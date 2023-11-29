dcc7a43185361d4e854dacb83a6a58f9fafbe1be
Maxime CAT <maximecat@outlook.fr>
2023-11-29 12:17:38 +0100

    service add 3 method,login,getUser,updateUser

--- src/utils/services/ApiService.js
+++ src/utils/services/ApiService.js
@@ -1,5 +1,34 @@
-class ApiService {
-
+export function login(email, password) {
+    return fetch('http://localhost:3001/api/v1/user/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+        .then(res => res.json())
 }
 
-export default ApiService
+export function getUser(token) {
+    return fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token
+        }
+    })
+        .then(res => res.json())
+}
+
+export function updateUser(token, body) {
+    return fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token
+        },
+        body: JSON.stringify(body)
+    })
+        .then(res => res.json())
+}
