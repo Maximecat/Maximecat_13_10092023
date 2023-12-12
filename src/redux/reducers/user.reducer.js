bc2ad9df16ac2af88a863d7a22a880918133bf8d
Maxime CAT <maximecat@outlook.fr>
2023-12-12 12:02:44 +0100

    user reducer

---
+++ src/redux/reducers/user.reducer.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchAction } from "../actions/user.action";
+
+const initialState = {
+    firstName: "",
+    lastName: "",
+    errorMessage: ""
+};
+
+const userSlice = createSlice({
+    initialState,
+    name: "userState",
+    extraReducers(builder) {
+        builder
+            .addCase(fetchAction.pending, (state) => {
+                console.log("pending");
+                console.log({ ...state });
+            })
+            .addCase(fetchAction.fulfilled, (state, action) => {
+                console.log("fulfilled");
+                console.log(action.payload)
+                state.firstName = action.payload.firstName
+                state.lastName = action.payload.lastName
+                state.errorMessage = ""
+                console.log({ ...state });
+            })
+            .addCase(fetchAction.rejected, (state, action) => {
+                console.log("rejected");
+                state.errorMessage = action.payload
+                console.log({ ...state });
+            })
+    }
+})
+
+export default userSlice.reducer
