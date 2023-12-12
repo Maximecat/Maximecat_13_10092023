5ef509d7662f25e8320995a423f08e743c38a719
Maxime CAT <maximecat@outlook.fr>
2023-12-12 12:02:15 +0100

    auth reducer

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,9 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { loginAction } from "../actions/auth.action";
+
 const initialState = {
-
+    rememberMe: false,
+    errorMessage: "",
+    loading: false,
+    token: window.localStorage.getItem("token") || ""
 };
 
-function authReducer(state = initialState, action) {
+const authSlice = createSlice({
+    initialState,
+    name: "authState",
+    extraReducers(builder) {
+        builder
+            .addCase(loginAction.pending, (state) => {
+                console.log("pending");
+                state.loading = true
+                console.log({ ...state });
+            })
+            .addCase(loginAction.fulfilled, (state, action) => {
+                console.log("fulfilled");
+                window.localStorage.setItem("token", action.payload.token)
+                state.errorMessage = ""
+                state.loading = false
+                state.token = action.payload.token
+                console.log({ ...state });
+            })
+            .addCase(loginAction.rejected, (state, action) => {
+                console.log("rejected");
+                state.errorMessage = action.payload
+                state.loading = false
+                console.log({ ...state });
+            })
+    }
+})
 
-};
-
-export { authReducer }
+export default authSlice.reducer
