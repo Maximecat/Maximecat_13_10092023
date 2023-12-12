1ab423bead943bb7b81318f08b7ad178c9ba19ac
Maxime CAT <maximecat@outlook.fr>
2023-12-12 12:04:14 +0100

    add form Header user page profil

--- src/components/HeaderUser/HeaderUser.jsx
+++ src/components/HeaderUser/HeaderUser.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
 import './HeaderUser.css';
+import { useState } from 'react';
+import { updateUser } from '../../utils/services/ApiService';
 
 function HeaderUser({ firstname, lastname }) {
     const [displayForm, setDisplayForm] = useState(false)
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [error, setError] = useState("")
 
     const editOrCancel = (e) => {
         setDisplayForm(!displayForm)
@@ -12,6 +16,17 @@ function HeaderUser({ firstname, lastname }) {
 
     const saveNewName = (e) => {
         console.log("Nouveau nom et prénom sauvegarder");
+        console.log(firstName, lastName);
+        updateUser(window.sessionStorage.getItem("token") || window.localStorage.getItem("token"), { firstName, lastName })
+            .then(res => {
+                console.log(res);
+                setFirstName(res.body.firstName)
+                setLastName(res.body.lastName)
+                setDisplayForm(false)
+            })
+            .catch(() => {
+                setError("An error occured")
+            })
     }
 
     return (
@@ -21,10 +36,11 @@ function HeaderUser({ firstname, lastname }) {
                     displayForm ?
                         <>
                             <h1>Welcome back</h1>
-                            <form className='forminputs-section'>
-                                <input type="text" id="firstname" placeholder={firstname} />
-                                <input type="text" id="lastname" placeholder={lastname} />
+                            <form className='forminputs-section' onSubmit={saveNewName}>
+                                <input type="text" id="firstname" value={firstName} placeholder={firstname} onChange={(e) => setFirstName(e.target.value || firstName)} />
+                                <input type="text" id="lastname" value={lastName} placeholder={lastname} onChange={(e) => setLastName(e.target.value || lastName)} />
                             </form>
+                            <p>{error}</p>
                             <div className='formbuttons-section'>
                                 <button onClick={saveNewName}>Save</button>
                                 <button onClick={editOrCancel}>Cancel</button>
