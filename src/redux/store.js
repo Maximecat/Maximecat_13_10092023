00b7b41269c9599bfb67e0d987d2deffe1b40a47
Maxime CAT <maximecat@outlook.fr>
2023-12-12 12:01:06 +0100

    store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-// import { configureStore } from '@reduxjs/toolkit'
-// import { authReducer } from './reducers/auth.reducer'
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './reducers/auth.reducer'
+import userReducer from './reducers/user.reducer'
 
-// const store = configureStore({
-//     reducer: {
-//         auth: authReducer,
-//     },
-// })
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        user: userReducer
+    },
+})
 
-// export { store }
+export { store }
