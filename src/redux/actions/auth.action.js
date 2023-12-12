868a1b65296cfdab67809c7d71cafb1d6cf23deb
Maxime CAT <maximecat@outlook.fr>
2023-12-12 12:02:26 +0100

    auth action

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,14 +1,18 @@
-import { createAction, createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit"
 import { login } from "../../utils/services/ApiService";
 
 const loginAction = createAsyncThunk(
     'auth/login',
-    async ({ email, password }) => {
-        console.log('coucou', email, password)
+    async ({ email, password }, thunkApi) => {
         const res = await login(email, password)
+        if (res.status === 200) {
+            console.log('reussite', email, password)
+            return res.body
+        } else {
+            console.log('echec', email, password)
+            return thunkApi.rejectWithValue(res.message)
+        }
     }
 );
 
-const logoutAction = createAction('auth/logout');
-
-export { loginAction, logoutAction }
+export { loginAction }
