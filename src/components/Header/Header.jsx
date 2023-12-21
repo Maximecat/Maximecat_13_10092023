42889916b57d9d467e3ade450418c3690b37dcba
Maxime CAT <maximecat@outlook.fr>
2023-12-21 08:44:45 +0100

    Header add dispatch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import './Header.css'
 import argentBankLogo from '../../pages/Home/argentBankLogo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutAction } from '../../redux/actions/auth.action';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const firstName = useSelector((state) => state.user.firstName)
+    const token = useSelector((state) => state.auth.token)
+    const logout = () => {
+        if (token) {
+            dispatch(logoutAction())
+            navigate("/")
+        }
+    }
+
     return (
         <nav className="main-nav">
             <a className="main-nav-logo" href="/">
@@ -12,10 +27,11 @@ function Header() {
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </a>
-            <div>
-                <a className="main-nav-item" href="/login">
+            <div className='user-info'>
+                <span>{token ? firstName : ""}</span>
+                <a className="main-nav-item" href={token ? "#" : "/login"} onClick={logout}>
                     <i className="fa fa-user-circle"></i>
-                    Sign In
+                    {token ? "Sign Out" : "Sign In"}
                 </a>
             </div>
         </nav>
